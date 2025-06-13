aca8175fdd4dd80542cd740e91f91284b3de0342
Francisco Perez <francisco.perez@alu.ing.unlp.edu.ar>
2025-06-13 18:43:44 -0300

    test gitLeak 2

--- src/app.js
+++ src/app.js
@@ -2,9 +2,8 @@ const express = require('express');
 const app = express();
 
 
-const API_KEY = 'ghp_a1b2c3d4e5f6g7h8i9j0klmnopqrstuvwx12';
 const extraVar = 'Esta variable ya no se usa, pero alguien olvidó borrarla!'
-
+const API_KEY = 'ghp_a1b2c3d4e5f6g7h8i9j0klmnopqrstuvwx12';
 app.use(express.json());
 
 app.get('/', (req, res) => {
